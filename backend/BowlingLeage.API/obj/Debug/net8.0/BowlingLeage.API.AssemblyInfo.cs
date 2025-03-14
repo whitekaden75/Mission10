48c68b00c7f8e274107f5007a6177e2be7420ad7
whitekaden75 <whitekaden75@gmail.com>
2025-03-14 17:40:54 -0600

    Initial Commit

--- backend/BowlingLeage.API/obj/Debug/net8.0/BowlingLeage.API.AssemblyInfo.cs
+++ backend/BowlingLeage.API/obj/Debug/net8.0/BowlingLeage.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BowlingLeage.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80948952e5167b2632204b14fe5abb32e5960587")]
 [assembly: System.Reflection.AssemblyProductAttribute("BowlingLeage.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BowlingLeage.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
